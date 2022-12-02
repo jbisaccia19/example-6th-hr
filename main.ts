78bb450af76aba477be5a5144ac994e91511096b
jbisaccia19 <119688482+jbisaccia19@users.noreply.github.com>
2022-12-02 12:43:15 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+scene.setBackgroundColor(5)
+effects.starField.startScreenEffect()
+pause(2000)
+game.setDialogFrame(img`
+    ..bbbbbbbbbbbbbbbbbbbb..
+    .bd111111111111111111db.
+    bd1dbbbbbbbbbbbbbbbbd1db
+    b1dbbbbbbbbbbbbbbbbbbd1b
+    b1bd1111111111111111db1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1b111111111111111111b1b
+    b1bd1111111111111111db1b
+    bd1bbbbbbbbbbbbbbbbbb1db
+    bbd111111111111111111dbb
+    .bbbbbbbbbbbbbbbbbbbbbb.
+    ..bbbbbbbbbbbbbbbbbbbb..
+    `)
+game.setDialogTextColor(10)
+game.showLongText("WELCOME TO MR.B'S EXAMPLE!", DialogLayout.Center)
+effects.starField.endScreenEffect()
+music.powerUp.playUntilDone()
